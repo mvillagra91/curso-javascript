d2ac69d1def069c27abf787dab112354052c0b6c
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-13 02:27:49 -0300

    Added Archive Variables

---
+++ Basic/01-variables.js
@@ -0,0 +1,21 @@
+//var -> Actualmente en desuso
+var helloWorld = "¡Hola, Javascript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript"
+console.log(helloWorld)
+
+//let -> Se puede cambiar la variable a diferencia de const
+let helloWorld2 = "¡Hola, Javascript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+//const -> La variable no puede ser reasignada
+
+const helloWorld3 = "¡Hola, Javascript 3!"
+console.log(helloWorld3)
+
+helloWorld3 = "¡Hola de nuevo, JavaScript 3!"//<- Ejemplo
+console.log(helloWorld3)
