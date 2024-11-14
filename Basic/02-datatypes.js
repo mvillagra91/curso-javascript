65fe3e0f73179ca567c38c34c5324476ef84ba58
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-13 21:52:04 -0300

    Add Datatypes

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,42 @@
+//Tipos de datos primitivos -> Utilizar console.log(); si deseas ver que trae la variable
+
+//Cadena de texto (string)
+let name = "Mario Villagra"
+let alias = 'Mariiotte'
+let email = `mvillagra.lorca@gmail.com`
+
+//Numeros (number)
+let age = 33        //Entero
+let altura = 1.67   //Decimal
+
+//Booleanos (boolean) -> True or false
+let isStudent = true
+let isTeacher = false
+
+//Undefined
+let Undefined
+
+//Null
+let nullValue = null //Representa ausencia de valor
+
+//Symbol
+let mySymbol = Symbol("mysymbol")
+
+//BigInt
+
+let myBigInt = BigInt(23153415345432153213543130)
+let myBigInt2 = 15631523156156151541614n
+
+//typeof muestra el tipo de dato)
+console.log(typeof(name))
+console.log(typeof(alias))
+console.log(typeof(email))
+console.log(typeof(age))
+console.log(typeof(altura))
+console.log(typeof(isStudent))
+console.log(typeof(isTeacher))
+console.log(typeof(Undefined))
+console.log(typeof(nullValue))
+console.log(typeof(mySymbol))
+console.log(typeof(myBigInt))
+console.log(typeof(myBigInt2))
