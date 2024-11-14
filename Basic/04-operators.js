9b0cb1850fffd62b08df1ddd2809f6d150794861
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-13 21:52:42 -0300

    Add operators

---
+++ Basic/04-operators.js
@@ -0,0 +1,104 @@
+// Operadores AritmÃ©ticos
+
+let a = 5
+let b = 10
+
+console.log(a + b) // Suma
+console.log(a - b) // Resta
+console.log(a * b) // MultiplicaciÃ³n
+console.log(a / b) // DivisiÃ³n
+
+console.log(a % b) // MÃ³dulo
+console.log(a ** b) // Exponente
+
+a++ // Incremento
+console.log(a)
+
+b-- // Decremento
+console.log(b)
+
+// Operadores de asignacion
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2 // Suma con asignaciÃ³n
+console.log(myVariable)
+
+myVariable -= 2 // Resta con asignaciÃ³n
+myVariable *= 2 // MultiplicaciÃ³n con asignaciÃ³n
+myVariable /= 2 // DivisiÃ³n con asignaciÃ³n
+myVariable %= 2 // MÃ³dulo con asignaciÃ³n
+myVariable **= 2 // Exponente con asignaciÃ³n
+
+// Operadores de comparaciÃ³n
+
+console.log(a > b) // Mayor que
+console.log(a < b) // Menor que
+console.log(a >= b) // Mayor o igual que
+console.log(a <= b) // Menor o igual que
+console.log(a == b) // Igualdad por valor
+console.log(a == 6)
+console.log(a == "6")
+console.log(a == a)
+console.log(a === a) // Igualdad por identidad (por tipo y valor) o igualdad estricta
+console.log(a === 6)
+console.log(a === "6")
+console.log(a != 6) // Desigualdad por valor
+console.log(a !== "6") // Desigualdad por identidad (por tipo y valor) o desigualdad estricta
+console.log(0 == false)
+console.log(1 == false)
+console.log(2 == false)
+console.log(0 == "")
+console.log(0 == " ")
+console.log(0 == '')
+console.log(0 == "Hola")
+console.log(0 === "")
+console.log(undefined == null)
+console.log(undefined === null)
+
+/*
+Truthy values (valores verdaderos)
+
+- Todos los numeros positivos y negativos menos el cero
+- Todas las cadenas de texto menos las vacÃ­as
+- El boolean true
+*/
+
+/*
+Falsy values (valores falsos)
+
+- 0
+-  0n
+- null
+- undefined
+- NaN (Not a Number)
+- El boolean false
+- Cadenas de texto vacÃ­as 
+*/
+
+// Operadores lÃ³gicos
+
+// and (&&)
+console.log(5 > 10 && 15 > 20)
+console.log(5 < 10 && 15 < 20)
+console.log(5 < 10 && 15 > 20)
+console.log(5 > 10 && 15 > 20 && 30 > 40)
+
+// or (||)
+console.log(5 > 10 || 15 > 20)
+console.log(5 < 10 || 15 < 20)
+console.log(5 < 10 || 15 > 20)
+console.log(5 > 10 || 15 > 20 || 30 > 40)
+
+console.log(5 > 10 && 15 > 20 || 30 < 40)
+
+// not (!)
+console.log(!true)
+console.log(!false)
+console.log(!(5 > 10 && 15 > 20))
+console.log(!(5 > 10 || 15 > 20))
+
+// Operadores ternarios
+
+const isRaining = false
+isRaining ? console.log("EstÃ¡ lloviendo") : console.log("No estÃ¡ lloviendo")
