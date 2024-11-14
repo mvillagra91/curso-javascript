f787d430793cd3337afced3a5bd9b700df0c44ee
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-13 21:51:14 -0300

    Add Prints

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,1 +1,12 @@
+//Imprimiendo hola mundo por consola
 console.log("¿Hola, JavaScript!")
+
+//Funciones numericas
+console.log("5");
+console.log(5);
+console.log(5+2);
+console.log(5-2);
+console.log(5*2);
+console.log(5/2);
+console.log(5%2);
+console.log(5**2);
